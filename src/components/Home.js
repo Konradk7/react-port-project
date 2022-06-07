d21203f4c4165913e561131b3912223ce30e70b9
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-06-07 19:26:14 +0200

    Position header photo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import HomeHeader from "./HomeHeader";
+import Items from "../assets/Hero Image@2x.png"
 
 function Home(props) {
     return (
-        <div>
+        <div style={{display: "flex", position: "relative"}}>
+            <img src={Items} alt="items" style={{height: "700px", position: "relative", left: "-350px"}}/>
             <HomeHeader />
         </div>
     );
