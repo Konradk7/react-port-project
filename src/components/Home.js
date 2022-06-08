35cfbcc1398c5ebf6256685ae39196fea258ed67
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-06-08 23:23:19 +0200

    Finish initial app-head

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import Items from "../assets/Hero Image@2x.png"
 function Home(props) {
     return (
         <div style={{display: "flex"}}>
-            {/*<img src={Items} alt="items" style={{height: "700px", position: "relative", left: "-350px"}}/>*/}
+            <img src={Items} alt="items" style={{height: "700px", position: "relative", left: "-320px", width: "950px"}}/>
             <HomeHeader />
         </div>
     );
--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Decoration from '../assets/Decoration.svg'
 
 function HomeHeader() {
     return (
@@ -16,7 +17,16 @@ function HomeHeader() {
                     <li><a href="#">Kontakt</a></li>
                 </ul>
             </nav>
-            <h1 className="home-nav__container--title">Zacznij pomagać! Oddaj niechciane rzeczy w zaufane ręce</h1>
+            <div className="home-header__container">
+                <h1 className="home-header__container--title">Zacznij pomagać!
+                    <br/> Oddaj niechciane rzeczy w zaufane ręce
+                </h1>
+                <img src={Decoration} alt="decoration" style={{marginLeft: "180px", marginTop: "20px"}}/>
+                <div>
+                    <a href="#">Oddaj rzeczy</a>
+                    <a href="#">Zorganizuj zbiórkę</a>
+                </div>
+            </div>
         </div>
     );
 }
