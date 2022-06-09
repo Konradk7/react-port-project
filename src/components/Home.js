c4223f0f1e63717f795a9895818c7b8c7338eb4b
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-06-09 18:50:18 +0200

    Add yellow path

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import HomeHeader from "./HomeHeader";
 import Items from "../assets/Hero Image@2x.png"
+import HomeHeader from "./HomeHeader";
+import StatisticsHome from "./StatisticsHome";
 
 function Home(props) {
     return (
-        <div style={{display: "flex"}}>
-            <img src={Items} alt="items" style={{height: "800px", position: "relative", left: "-320px", width: "930px"}}/>
-            <HomeHeader />
+        <div>
+            <div style={{display: "flex"}}>
+                <img src={Items} alt="items"
+                     style={{height: "800px", position: "relative", left: "-320px", width: "930px"}}/>
+                <HomeHeader/>
+            </div>
+            <StatisticsHome/>
         </div>
     );
 }
---
+++ src/components/StatisticsHome.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function StatisticsHome(props) {
+    return (
+        <div className="statistics__container">
+            <section>
+                <h2>10</h2>
+                <h3>ODDANYCH WORKÓW</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+            </section>
+
+            <section>
+                <h2>5</h2>
+                <h3>WSPARTYCH ORGANIZACJI</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+            </section>
+
+            <section>
+                <h2>7</h2>
+                <h3>ZORGANIZOWANYCH ZBIÓREK</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+            </section>
+        </div>
+    );
+}
+
+export default StatisticsHome;
