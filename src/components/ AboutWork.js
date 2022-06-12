8a5493ff6ccbe31f3b33a60a0720dbfcfa940878
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-06-12 04:13:30 +0200

    Add about work

--- src/components/ AboutWork.js
+++ src/components/ AboutWork.js
@@ -8,37 +8,41 @@ function AboutWork() {
             <img src={Decoration} alt="decoration" className="about-work__container--decoration"/>
             <div className="about-work__container--links">
                 <a href="#">Fundacjom</a>
-                <a href="#" style={{display: "flex", flexDirection: "column"}}><span>Organizacjom</span> <span>pozarządowym</span></a>
-                <a href="#" style={{display: "flex", flexDirection: "column"}}><span>Lokalnym</span> <span>zbiórkom</span></a>
+                <a href="#" style={{display: "flex", flexDirection: "column"}}><span>Organizacjom</span>
+                    <span>pozarządowym</span></a>
+                <a href="#" style={{display: "flex", flexDirection: "column"}}><span>Lokalnym</span>
+                    <span>zbiórkom</span></a>
             </div>
-            <p>
+            <p className="about-work__container--main">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 Ut enim ad minim veniam, quis nostrud exercitation.
             </p>
-            <div>
+            <section>
                 <div>
                     <h2>Organizacja “Lorem Ipsum 1”</h2>
                     <p>Quis varius quam quisque id diam vel quam elementum pulvinar.</p>
                 </div>
                 <p>Egestas, sed, tempus</p>
-            </div>
-            <div>
+            </section>
+            <section>
                 <div>
                     <h2>Organizacja “Lorem Ipsum 2”</h2>
                     <p>Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.</p>
                 </div>
                 <p>Ut, aliquam, purus, sit, amet</p>
-            </div>
-            <div>
+            </section>
+            <section>
                 <div>
                     <h2>Organizacja “Lorem Ipsum 3”</h2>
                     <p>Scelerisque in dictum non consectetur a erat nam.</p>
                 </div>
                 <p>Mi, quis, hendrerit, dolor</p>
+            </section>
+            <div className="about-work__container--footer">
+                <a href="#">1</a>
+                <a href="#">2</a>
             </div>
-            <a href="#">1</a>
-            <a href="#">2</a>
         </div>
     );
 }
