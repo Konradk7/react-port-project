038aada9fa6a97726101c6ec0c05d96de2578cde
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-06-12 06:18:25 +0200

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,38 +5,36 @@ import Instagram from "../assets/Instagram.svg";
 
 function Footer() {
     return (
-        <div>
-            <div>
-                <h2>Skontaktuj się z nami</h2>
-                <img src={Decoration} alt="decoration"/>
-            </div>
-            <form>
-                <div>
-                    <div>
-                        <label htmlFor="name">Wpisz swoje imię</label>
-                        <input type="text" placeholder="Krzysztof" name="name"/>
-                    </div>
-                    <div>
-                        <label htmlFor="email">Wpisz swój email</label>
-                        <input type="email" placeholder="abc@xyz.pl" name="email"/>
-                    </div>
+        <div className="footer__container">
+            <div className="footer__container--main">
+                <div className="footer__container--header">
+                    <h2>Skontaktuj się z nami</h2>
+                    <img src={Decoration} alt="decoration"/>
                 </div>
-                <div>
-                    <label htmlFor="message">Wpisz swoją wiadomość</label>
-                    <input
-                        type="text"
-                        placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                        name="message"
-                    />
-                </div>
-                <button>Wyślij</button>
-            </form>
-            <div>
-                <p>Copyright by Coders Lab</p>
-                <img src={Facebook} alt="facebook icon"/>
-                <img src={Instagram} alt="instagram icon"/>
+                <form>
+                    <div className="footer-form__container">
+                        <section>
+                            <label htmlFor="name">Wpisz swoje imię</label>
+                            <input type="text" placeholder="Krzysztof" name="name"/>
+                        </section>
+                        <section>
+                            <label htmlFor="email">Wpisz swój email</label>
+                            <input type="email" placeholder="abc@xyz.pl" name="email"/>
+                        </section>
+                    </div>
+                    <div className="footer-form__main">
+                        <label htmlFor="message">Wpisz swoją wiadomość</label>
+                        <textarea
+                            placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                            name="message"
+                        />
+                    </div>
+                    <div className="footer-form__button">
+                        <button>Wyślij</button>
+                    </div>
+                </form>
             </div>
         </div>
     );
