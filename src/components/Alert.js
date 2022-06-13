bcde5a849332912677b89fe487a4f581babb5edf
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-06-13 07:21:04 +0200

    Add alert to give-items

---
+++ src/components/Alert.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Alert() {
+    return (
+        <div className="alert">
+            <div className="alert__container">
+                <h2>Ważne!</h2>
+                <p>Uzupełnij szczegóły dotyczące Twoich rzeczy. Dzięki temu będziemy wiedzieć komu najlepiej je
+                    przekazać.</p>
+            </div>
+        </div>
+    );
+}
+
+export default Alert;
--- src/components/GiveItems.js
+++ src/components/GiveItems.js
@@ -1,42 +1,13 @@
 import React from 'react';
-import Navigation from "./Navigation";
-import Decoration from "../assets/Decoration.svg";
+
+import GiveItemsHead from "./GiveItemsHead";
+import Alert from "./Alert";
 
 function GiveItems() {
     return (
-        <div className="give-items">
-            <div className="give-items__header">
-                <Navigation/>
-            </div>
-            <div className="give-items__main">
-                <h1>Oddaj rzeczy, których już nie chcesz <br/> POTRZEBUJĄCYM</h1>
-                <div className="give-items__main--boxes">
-                    <img src={Decoration} alt="decoration"/>
-                    <h2>Wystarczą cztery proste kroki:</h2>
-                    <div className="give-items__boxes">
-                        <section>
-                            <h3>1</h3>
-                            <p>Wybierz rzeczy</p>
-                            <div />
-                        </section>
-                        <section>
-                            <h3>2</h3>
-                            <p>Spakuj je w worki</p>
-                            <div />
-                        </section>
-                        <section>
-                            <h3>3</h3>
-                            <p>Wybierz fundację</p>
-                            <div />
-                        </section>
-                        <section>
-                            <h3>4</h3>
-                            <p>Zamów kuriera</p>
-                            <div />
-                        </section>
-                    </div>
-                </div>
-            </div>
+        <div>
+            <GiveItemsHead />
+            <Alert />
         </div>
     );
 }
