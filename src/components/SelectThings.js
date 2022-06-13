74754bb0cbd25ee09dbdcb15666663eeece4b082
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-06-13 08:00:43 +0200

    Add Select

--- src/components/SelectThings.js
+++ src/components/SelectThings.js
@@ -2,22 +2,34 @@ import React from 'react';
 
 function SelectThings() {
     return (
-        <div>
-            <h4>Krok 1/4</h4>
-            <h2>Zaznacz co chcesz oddać:</h2>
-            <form>
-                <input type="checkbox" name="first-input"/>
-                <label htmlFor="first-input">ubrania, które nadają się do ponownego użycia</label>
-                <input type="checkbox" name="second-input"/>
-                <label htmlFor="second-input">ubrania, do wyrzucenia</label>
-                <input type="checkbox" name="third-input"/>
-                <label htmlFor="third-input">zabawki</label>
-                <input type="checkbox" name="fourth-input"/>
-                <label htmlFor="fourth-input">książki</label>
-                <input type="checkbox" name="fifth-input"/>
-                <label htmlFor="fifth-input">inne</label>
-            </form>
-            <button>Dalej</button>
+        <div className="select-things">
+            <div className="select-things__container">
+                <h4>Krok 1/4</h4>
+                <h2>Zaznacz co chcesz oddać:</h2>
+                <form>
+                    <section>
+                        <input type="checkbox" name="first-input"/>
+                        <label htmlFor="first-input">ubrania, które nadają się do ponownego użycia</label>
+                    </section>
+                    <section>
+                        <input type="checkbox" name="second-input"/>
+                        <label htmlFor="second-input">ubrania, do wyrzucenia</label>
+                    </section>
+                    <section>
+                        <input type="checkbox" name="third-input"/>
+                        <label htmlFor="third-input">zabawki</label>
+                    </section>
+                    <section>
+                        <input type="checkbox" name="fourth-input"/>
+                        <label htmlFor="fourth-input">książki</label>
+                    </section>
+                    <section>
+                        <input type="checkbox" name="fifth-input"/>
+                        <label htmlFor="fifth-input">inne</label>
+                    </section>
+                </form>
+                <button>Dalej</button>
+            </div>
         </div>
     );
 }
