6ffd7f4d37ffc86e106e29a10849e63a67e36763
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-06-13 07:11:56 +0200

    Add give-items

--- src/components/GiveItems.js
+++ src/components/GiveItems.js
@@ -4,27 +4,37 @@ import Decoration from "../assets/Decoration.svg";
 
 function GiveItems() {
     return (
-        <div className="give-items__header">
-            <Navigation />
-            <div>
-                <h1>Oddaj rzeczy, których już nie chcesz POTRZEBUJĄCYM</h1>
-                <img src={Decoration} alt="decoration"/>
-                <h2>Wystarczą cztery proste kroki:</h2>
-                <div>
-                    <h3>1</h3>
-                    <p>Wybierz rzeczy</p>
-                </div>
-                <div>
-                    <h3>2</h3>
-                    <p>Spakuj je w worki</p>
-                </div>
-                <div>
-                    <h3>3</h3>
-                    <p>Wybierz fundację</p>
-                </div>
-                <div>
-                    <h3>4</h3>
-                    <p>Zamów kuriera</p>
+        <div className="give-items">
+            <div className="give-items__header">
+                <Navigation/>
+            </div>
+            <div className="give-items__main">
+                <h1>Oddaj rzeczy, których już nie chcesz <br/> POTRZEBUJĄCYM</h1>
+                <div className="give-items__main--boxes">
+                    <img src={Decoration} alt="decoration"/>
+                    <h2>Wystarczą cztery proste kroki:</h2>
+                    <div className="give-items__boxes">
+                        <section>
+                            <h3>1</h3>
+                            <p>Wybierz rzeczy</p>
+                            <div />
+                        </section>
+                        <section>
+                            <h3>2</h3>
+                            <p>Spakuj je w worki</p>
+                            <div />
+                        </section>
+                        <section>
+                            <h3>3</h3>
+                            <p>Wybierz fundację</p>
+                            <div />
+                        </section>
+                        <section>
+                            <h3>4</h3>
+                            <p>Zamów kuriera</p>
+                            <div />
+                        </section>
+                    </div>
                 </div>
             </div>
         </div>
