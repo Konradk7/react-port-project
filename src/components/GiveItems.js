784fbd8530cd7f4ffb09ac9af40702dcecb9d576
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-06-13 07:30:26 +0200

    Init select file

--- src/components/GiveItems.js
+++ src/components/GiveItems.js
@@ -1,13 +1,14 @@
 import React from 'react';
-
 import GiveItemsHead from "./GiveItemsHead";
 import Alert from "./Alert";
+import SelectThings from "./SelectThings";
 
 function GiveItems() {
     return (
         <div>
             <GiveItemsHead />
             <Alert />
+            <SelectThings />
         </div>
     );
 }
