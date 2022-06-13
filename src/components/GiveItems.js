60d4a7640adf3b2cdb060d423b86daa4520590de
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-06-13 06:33:49 +0200

    Init jsx of give-item

--- src/components/GiveItems.js
+++ src/components/GiveItems.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import Navigation from "./Navigation";
+import Decoration from "../assets/Decoration.svg";
 
 function GiveItems() {
     return (
-        <div>
-
+        <div className="give-items__header">
+            <Navigation />
+            <div>
+                <h1>Oddaj rzeczy, których już nie chcesz POTRZEBUJĄCYM</h1>
+                <img src={Decoration} alt="decoration"/>
+                <h2>Wystarczą cztery proste kroki:</h2>
+                <div>
+                    <h3>1</h3>
+                    <p>Wybierz rzeczy</p>
+                </div>
+                <div>
+                    <h3>2</h3>
+                    <p>Spakuj je w worki</p>
+                </div>
+                <div>
+                    <h3>3</h3>
+                    <p>Wybierz fundację</p>
+                </div>
+                <div>
+                    <h3>4</h3>
+                    <p>Zamów kuriera</p>
+                </div>
+            </div>
         </div>
     );
 }
