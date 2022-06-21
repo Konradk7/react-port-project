564d843289721210f070bc79c83dd3b1de8eef5e
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-06-21 21:38:01 +0200

    rebuild build

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -12,7 +12,7 @@ function Navigation() {
                     <li><Link to="/register">Załóż konto</Link></li>
                 </ul>
                 <ul className="home-nav__container--list">
-                    <li><Link to="/">Start</></li>
+                    <li><Link to="/">Start</Link></li>
                     <li><ScrollLink to="settings" smooth={true} duration={1000}>O co chodzi?</ScrollLink></li>
                     <li><ScrollLink to="about-us" smooth={true} duration={1000}>O nas</ScrollLink></li>
                     <li><ScrollLink to="about-work" smooth={true} duration={1000}>Fundacja i organizacje</ScrollLink></li>
