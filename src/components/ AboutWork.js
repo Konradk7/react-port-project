dd4090d9c23355a68d3b94ff4bb6033586d5bc3e
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-06-26 23:32:51 +0200

    Add pagination <3

--- src/components/ AboutWork.js
+++ src/components/ AboutWork.js
@@ -5,11 +5,7 @@ import Decoration from '../assets/Decoration.svg';
 
 
 function AboutWork() {
-    const [who, setWho] = useState(0);
-    const [pageNumber, setPageNumber] = useState(0);
 
-    const infoPerPage = 3;
-    const pagesVisited = pageNumber * infoPerPage;
     return (
         <div className="about-work__container" id="about-work">
             <h2 className="about-work__container--title">Komu pomagamy?</h2>
@@ -22,10 +18,7 @@ function AboutWork() {
                     <span>zbiórkom</span></h3>
             </div>
             <AboutWorkText />
-            <div className="about-work__container--footer">
-                <button>1</button>
-                <button>2</button>
-            </div>
+
         </div>
     );
 }
--- src/components/AboutWorkText.js
+++ src/components/AboutWorkText.js
@@ -1,40 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
+import FoundationJson from "../mock_data/foundations.json";
+import ReactPaginate from "react-paginate";
 
-function AboutWorkText(key, value, checked) {
+function AboutWorkText() {
+    const [foundations, setFoundations] = useState(FoundationJson.slice(0, 6));
+    const [pageNumber, setPageNumber] = useState(0);
 
+    const infoPerPage = 3;
+    const pagesVisited = pageNumber * infoPerPage;
+    const pageCount = Math.ceil(foundations.length / infoPerPage)
 
+    const changePage = ({selected}) => {
+        setPageNumber(selected)
+    }
+
+    const displayPage = foundations
+        .slice(pagesVisited, pagesVisited + infoPerPage)
+        .map((data) => {
+            return (
+                <>
+                    <p
+                        className="about-work__container--main">
+                        {data.main}
+                    </p>
+                    <section >
+                        <div>
+                            <h2>{data.title}</h2>
+                            <p>{data.text}</p>
+                        </div>
+                        <p>{data.aside}</p>
+                    </section>
+                </>
+            )
+        })
     return (
         <>
-            <p className="about-work__container--main">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation.
-            </p>
-            <section>
-                <div>
-                    <h2>Organizacja “Lorem Ipsum 1”</h2>
-                    <p>Quis varius quam quisque id diam vel quam elementum pulvinar.</p>
-                </div>
-                <p>Egestas, sed, tempus</p>
-            </section>
-            <section>
-                <div>
-                    <h2>Organizacja “Lorem Ipsum 2”</h2>
-                    <p>Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.</p>
-                </div>
-                <p>Ut, aliquam, purus, sit, amet</p>
-            </section>
-            <section>
-                <div>
-                    <h2>Organizacja “Lorem Ipsum 3”</h2>
-                    <p>Scelerisque in dictum non consectetur a erat nam.</p>
-                </div>
-                <p>Mi, quis, hendrerit, dolor</p>
-            </section>
-
+            {displayPage}
+            <ReactPaginate
+                previousLabel={""}
+                nextLabel={""}
+                pageCount={pageCount}
+                onPageChange={changePage}
+                containerClassName={"about-work__container--footer"}
+            />
         </>
+    )
 
-    );
+
 }
 
 export default AboutWorkText;
