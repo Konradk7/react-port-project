fe51b5f16ceeaf812d9858512208ea5b57426c88
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-06-27 22:51:02 +0200

    Save paginations

--- src/components/AboutWorkText.js
+++ src/components/AboutWorkText.js
@@ -1,9 +1,13 @@
 import React, {useState} from 'react';
 import FoundationJson from "../mock_data/foundations.json";
+import OrganizationJson from "../mock_data/organizations.json";
+import LocalJson from "../mock_data/local.json";
 import ReactPaginate from "react-paginate";
 
 function AboutWorkText(state) {
     const [foundations, setFoundations] = useState(FoundationJson.slice(0, 6));
+    const [organizations, setOrganizations] = useState(OrganizationJson.slice(0, 6));
+    const [local, setLocal] = useState(LocalJson.slice(0, 6));
     const [pageNumber, setPageNumber] = useState(0);
 
     const infoPerPage = 3;
@@ -14,7 +18,7 @@ function AboutWorkText(state) {
         setPageNumber(selected)
     }
 
-    const displayPage = foundations
+    const displayPage = organizations
         .slice(pagesVisited, pagesVisited + infoPerPage)
         .map((data) => {
             return (
