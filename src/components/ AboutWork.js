eafb5defb20dd5c9bf9ee8627872bf7c120f36ea
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-06-28 21:51:16 +0200

    Add more paginations

--- src/components/ AboutWork.js
+++ src/components/ AboutWork.js
@@ -4,20 +4,20 @@ import Decoration from '../assets/Decoration.svg';
 
 
 function AboutWork() {
-    const [state, setState] = useState(2);
+    const [organizations, setOrganizations] = useState(2);
 
     return (
         <div className="about-work__container" id="about-work">
             <h2 className="about-work__container--title">Komu pomagamy?</h2>
             <img src={Decoration} alt="decoration" className="about-work__container--decoration"/>
             <div className="about-work__container--links">
-                <h3 onClick={() => {setState(1)}}>Fundacjom</h3>
-                <h3 style={{display: "flex", flexDirection: "column"}} onClick={() => {setState(2)}}><span>Organizacjom</span>
+                <h3 onClick={() => {setOrganizations(1)}}>Fundacjom</h3>
+                <h3 style={{display: "flex", flexDirection: "column"}} onClick={() => {setOrganizations(2)}}><span>Organizacjom</span>
                     <span>pozarządowym</span></h3>
-                <h3 style={{display: "flex", flexDirection: "column"}} onClick={() => {setState(3)}}><span>Lokalnym</span>
+                <h3 style={{display: "flex", flexDirection: "column"}} onClick={() => {setOrganizations(3)}}><span>Lokalnym</span>
                     <span>zbiórkom</span></h3>
             </div>
-            <AboutWorkText state={state}/>
+            <AboutWorkText org={organizations}/>
 
         </div>
     );
--- src/components/AboutWorkText.js
+++ src/components/AboutWorkText.js
@@ -1,15 +1,15 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import FoundationJson from "../mock_data/foundations.json";
 import OrganizationJson from "../mock_data/organizations.json";
 import LocalJson from "../mock_data/local.json";
 import ReactPaginate from "react-paginate";
 
-function AboutWorkText(state) {
+function AboutWorkText({org}) {
     const [foundations, setFoundations] = useState(FoundationJson.slice(0, 9));
     const [organizations, setOrganizations] = useState(OrganizationJson.slice(0, 6));
     const [local, setLocal] = useState(LocalJson.slice(0, 6));
+    const [cur, setCur] = useState(foundations);
     const [pageNumber, setPageNumber] = useState(0);
-
     const infoPerPage = 3;
     const pagesVisited = pageNumber * infoPerPage;
     const pageCount = Math.ceil(foundations.length / infoPerPage)
@@ -18,11 +18,11 @@ function AboutWorkText(state) {
         setPageNumber(selected)
     }
 
-    const displayPage = foundations
+    const displayPage = (fund) => fund
         .slice(pagesVisited, pagesVisited + infoPerPage)
         .map((data) => {
             return (
-                <>
+                <div key={data.id}>
                     <p
                         className="about-work__container--main">
                         {data.main}
@@ -34,12 +34,25 @@ function AboutWorkText(state) {
                         </div>
                         <p>{data.aside}</p>
                     </section>
-                </>
+                </div>
             )
         })
+
+    useEffect(() => {
+        if(org === 1) {
+            setCur(foundations);
+        }
+        else if (org === 2) {
+            setCur(organizations);
+        }
+        else if (org === 3) {
+            setCur(local);
+        }
+    }, [org]);
+
     return (
         <>
-            {displayPage}
+            {displayPage(cur)}
             <ReactPaginate
                 previousLabel={""}
                 nextLabel={""}
