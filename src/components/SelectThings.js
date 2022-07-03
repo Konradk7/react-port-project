a1ad0d9261b204fbe93f7fa5276e80559b955082
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-07-03 06:27:18 +0200

    init form validation

--- src/components/SelectThings.js
+++ src/components/SelectThings.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SelectThingsOne from "./SelectThingsSteps/SelectThingsOne";
 import SelectThingsTwo from "./SelectThingsSteps/SelectThingsTwo";
 import SelectThingsThree from "./SelectThingsSteps/SelectThingsThree";
@@ -6,18 +6,25 @@ import SelectThingsFour from "./SelectThingsSteps/SelectThingsFour";
 import SelectThingsSubmit from "./SelectThingsSteps/SelectThingsSubmit";
 
 function SelectThings() {
+    const [step, setStep] = useState(1);
+
+    let show;
+    if(step === 1) {
+        show = <SelectThingsOne step={step}/>
+    }  else if(step === 2) {
+        show = <SelectThingsTwo step={step}/>
+    } else if(step === 3) {
+        show = <SelectThingsThree step={step}/>
+    } else if(step === 4) {
+        show = <SelectThingsFour step={step}/>
+    } else if(step === 5) {
+        show = <SelectThingsSubmit step={step}/>
+    }
+
     return (
         <div className="select-things">
             <div className="select-things__container">
-                <SelectThingsOne />
-                {/*<SelectThingsTwo />*/}
-                {/*<SelectThingsThree />*/}
-                {/*<SelectThingsFour />*/}
-                {/*<SelectThingsSubmit />*/}
-
-
-
-
+                {show}
             </div>
         </div>
     );
