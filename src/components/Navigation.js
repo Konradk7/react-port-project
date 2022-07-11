d846ba56feb557738d411e3d7d70c53a77d88ce2
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-07-11 12:01:34 +0200

    Add jsx and sass to hamb-menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from "react-router-dom"
 import {Link as ScrollLink} from 'react-scroll';
 import Hamburger from "../assets/icons8-menu.svg";
 
 
 function Navigation({user, logout}) {
+    const [showMenu, setShowMenu] = useState(false);
 
 
     return (
@@ -19,8 +20,9 @@ function Navigation({user, logout}) {
                 <li><ScrollLink to="about-us" smooth={true} duration={1000}>O nas</ScrollLink></li>
                 <li><ScrollLink to="about-work" smooth={true} duration={1000}>Fundacja i organizacje</ScrollLink></li>
                 <li><ScrollLink to="footer-form" smooth={true} duration={1000}>Kontakt</ScrollLink></li>
-                <div className="home-nav__container--menu hide"><img src={Hamburger} alt="menu"/></div>
             </ul>
+            <div className="home-nav__container--menu hide"><img src={Hamburger} alt="menu"/></div>
+
         </nav>
     );
 }
