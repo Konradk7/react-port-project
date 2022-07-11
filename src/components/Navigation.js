e555f6ccb732e443e5d55c9fcde4797a5f7fbb0d
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-07-11 12:07:41 +0200

    Add bttn to show menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,11 @@ import Hamburger from "../assets/icons8-menu.svg";
 
 
 function Navigation({user, logout}) {
-    const [showMenu, setShowMenu] = useState(false);
+    const [showMenu, setShowMenu] = useState(true);
+
+    const handleShowNav = () => {
+        setShowMenu(!showMenu)
+    }
 
 
     return (
@@ -14,14 +18,15 @@ function Navigation({user, logout}) {
                 <li>{user ? <div className="nav--logout"><button onClick={logout}>Wyloguj</button><h2>{user.email}</h2></div> : <Link to="/login">Zaloguj</Link>}</li>
                 <li><Link to="/register">Załóż konto</Link></li>
             </ul>
-            <ul className="home-nav__container--list">
+            {/*Przerobię to później na z ul na nav, wypadło mi z głowy*/}
+            {showMenu && <ul className="home-nav__container--list">
                 <li><Link to="/">Start</Link></li>
                 <li><ScrollLink to="settings" smooth={true} duration={1000}>O co chodzi?</ScrollLink></li>
                 <li><ScrollLink to="about-us" smooth={true} duration={1000}>O nas</ScrollLink></li>
                 <li><ScrollLink to="about-work" smooth={true} duration={1000}>Fundacja i organizacje</ScrollLink></li>
                 <li><ScrollLink to="footer-form" smooth={true} duration={1000}>Kontakt</ScrollLink></li>
-            </ul>
-            <div className="home-nav__container--menu hide"><img src={Hamburger} alt="menu"/></div>
+            </ul>}
+            <div className="home-nav__container--menu hide" onClick={handleShowNav}><img src={Hamburger} alt="menu"/></div>
 
         </nav>
     );
