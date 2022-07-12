b5540045d951196d7b2f3a6a09096425f90a3526
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-07-12 13:14:04 +0200

    Move nodemailer to another dir

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,28 +3,6 @@ import Decoration from "../assets/Decoration.svg";
 
 
 function Footer() {
-    const nodemailer = require("nodemailer");
-    async function main() {
-        let testAccount = await nodemailer.createTestAccount();
-
-        let transporter = nodemailer.createTransport({
-            host: "smtp.ethereal.email",
-            port: 587,
-            secure: false,
-            auth: {
-                user: testAccount.user,
-                pass: testAccount.pass,
-            },
-        });
-        let info = await transporter.sendMail({
-            from: '"Fred Foo 👻" <foo@example.com>', // sender address
-            to: "bar@example.com, baz@example.com", // list of receivers
-            subject: "Hello ✔", // Subject line
-            text: "Hello world?", // plain text body
-            html: "<b>Hello world?</b>", // html body
-        });
-    }
-    main().catch(console.error);
 
 
     return (
