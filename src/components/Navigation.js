8ac25752fa1af2c416bf500af7bd1eca4c364d97
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-07-16 18:36:01 +0200

    Rework width in give-items header

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import {Link} from "react-router-dom"
 import {Link as ScrollLink} from 'react-scroll';
-import Hamburger from "../assets/icons8-menu.svg";
 
 
 function Navigation({user, logout}) {
@@ -15,17 +14,21 @@ function Navigation({user, logout}) {
     return (
         <nav className="home-nav__container">
             <ul className="home-nav__container--header">
-                <li>{user ? <div className="nav--logout"><button onClick={logout}>Wyloguj</button><h2>{user.email}</h2></div> : <Link to="/login">Zaloguj</Link>}</li>
+                <li>{user ? <div className="nav--logout">
+                    <button onClick={logout}>Wyloguj</button>
+                    <h2>{user.email}</h2></div> : <Link to="/login">Zaloguj</Link>}</li>
                 <li><Link to="/register">Załóż konto</Link></li>
             </ul>
-            {/*Przerobię to później z ul na nav, wypadło mi z głowy*/}
-            {showMenu && <ul className="home-nav__container--list">
-                <li><Link to="/">Start</Link></li>
-                <li><ScrollLink to="settings" smooth={true} duration={1000}>O co chodzi?</ScrollLink></li>
-                <li><ScrollLink to="about-us" smooth={true} duration={1000}>O nas</ScrollLink></li>
-                <li><ScrollLink to="about-work" smooth={true} duration={1000}>Fundacja i organizacje</ScrollLink></li>
-                <li><ScrollLink to="footer-form" smooth={true} duration={1000}>Kontakt</ScrollLink></li>
-            </ul>}
+            <nav>
+                {showMenu && <ul className="home-nav__container--list">
+                    <li><Link to="/">Start</Link></li>
+                    <li><ScrollLink to="settings" smooth={true} duration={1000}>O co chodzi?</ScrollLink></li>
+                    <li><ScrollLink to="about-us" smooth={true} duration={1000}>O nas</ScrollLink></li>
+                    <li><ScrollLink to="about-work" smooth={true} duration={1000}>Fundacja i organizacje</ScrollLink>
+                    </li>
+                    <li><ScrollLink to="footer-form" smooth={true} duration={1000}>Kontakt</ScrollLink></li>
+                </ul>}
+            </nav>
             <div className="home-nav__container--menu hide" onClick={handleShowNav}></div>
 
         </nav>
